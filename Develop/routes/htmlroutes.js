484ed42c68a1a9cb7e798bc1b080625274ecdc8f
Sczygelski <avamariescz@gmail.com>
2022-10-16 20:20:26 -0500

    htmlroutes

---
+++ Develop/routes/htmlroutes.js
@@ -0,0 +1,12 @@
+const path = reqwuire('path')
+const router - require('express').Router()
+
+router.get('/notes', (req,res) => {
+    res.sendFile(path.join(__dirname, '../public/notes.html'))
+})
+
+router.get('*', (req,res) => {
+    res.sendFil(path.join(__dirname, '../public/index.html'))
+})
+
+module.exports=router
