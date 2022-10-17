561537cd7bcbfdc232d5699dbb5f8462dc8ab03f
Sczygelski <avamariescz@gmail.com>
2022-10-16 20:18:01 -0500

    post route

--- Develop/routes/apiroutes.js
+++ Develop/routes/apiroutes.js
@@ -3,11 +3,14 @@ const store = require('../db/store')
 const path = require('path')
 const {notes} = require('../db/db.json')
 
+
 router.get('/notes', (req,res) => {
     let allnotes = notes
     res.json(allnotes)
 })
 
-router.post('/notes', (req/res) = > {
-    
+router.post('/notes', (req,res) => {
+    req.body.id = notes.length.toString()
+    const note = postnote(req.body, notes)
+    res.json(note)
 })
--- Develop/server.js
+++ Develop/server.js
@@ -1,5 +1,4 @@
 const express = require('express')
-const fs = require('fs')
 
 const apiroutes = require('./routes/apiroutes')
 const htmlroutes = require('./ routes/htmlroutes')
