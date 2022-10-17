359864d9864952ef819fbf4ecc5bd5d3c2067fcf
Sczygelski <avamariescz@gmail.com>
2022-10-16 20:05:02 -0500

    server.js

---
+++ Develop/server.js
@@ -0,0 +1,19 @@
+const express = require('express')
+
+const apiroutes = require('./routes/apiroutes')
+const htmlroutes = require('./ routes/htmlroutes')
+
+const app = express()
+const PORT = process.env.PORT || 3001
+
+app.use(express.urlencoded({extended:true}))
+app.use(express.json)
+
+app.use(express.static('public'))
+
+app.use('/api',apiroutes)
+app.use('/',htmlroutes)
+
+app.listen(PORT, () => {
+    console.log(`App listening on PORT ${PORT}`)
+})
